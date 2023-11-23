f0d3d85051b670afbd7b7117ca2909390adb9460
egbert zhao <zhaoshengxuan108@163.com>
2023-11-23 15:07:54 +0800

    '博客'

---
+++ index.search.js
@@ -0,0 +1,42 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "home",
+    "content": "标题啊啊啊 Lorem Ipsum.\nInfo Info Info Info ",
+    "description": "",
+    "tags": null,
+    "title": "测试标题一",
+    "uri": "/basics/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Lorem Ipsum.\naaa aaaaaa\naaaa public static void main(String[] args) { AppMain appMain = new AppMain(); String path = \"E:\\\\book\"; File dirFile = new File(path); if (dirFile.isDirectory()) { File[] files = dirFile.listFiles(); for (File f : files) { String fileHeader = appMain.getFileHeader(f); String fileType = appMain.getFileType(fileHeader); if (StringUtils.isNotEmpty(fileType)) { //System.out.println(f.getName()); String fileName = f.getName() + \".\" + fileType; if (f.renameTo(new File(path + \"/\" + fileName))) { System.out.println(f.getName() + \" 重命名成功！\"); } else { System.err.println(f.getName() + \" 重命名失败！\"); } } } } else { System.out.println(dirFile.getAbsolutePath() + \" 不是路径\"); } }",
+    "description": "",
+    "tags": null,
+    "title": "home",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "home \u003e 测试标题一",
+    "content": "啊啊啊 I am rendered with bold text\n- [x] Basic Test\nOption Description data path to data files to supply the data that will be passed into templates. engine engine to be used for processing templates. Handlebars is the default. ext extension to be used for dest files. Upstage\nInfo An information disclaimer\nYou can add standard markdown syntax:\nmultiple paragraphs bullet point lists emphasized, bold and even bold emphasized text links etc. ...and even source code the possibilities are endless (almost - including other shortcodes may or may not work)\n",
+    "description": "",
+    "tags": null,
+    "title": "Second Content",
+    "uri": "/basics/second-content/index.html"
+  },
+  {
+    "breadcrumb": "home",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "home",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
